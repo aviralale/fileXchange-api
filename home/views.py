c5a32d32121bce1e996ed7b6ac0050c3adafd294
Aviral Ale <aviralale@gmail.com>
2023-12-25 21:05:58 +0545

    vercel modification for api

--- home/views.py
+++ home/views.py
@@ -16,9 +16,17 @@ from django.conf import settings
 
 class HandleFileUpload(APIView):
     parser_classes = [MultiPartParser]
+
     def post(self, request):
         try:
             data = request.data
+            # Check if the 'file' key is present in the request data
+            if 'file' not in data:
+                return Response({
+                    'status': 'error',
+                    'message': 'File not provided in the request',
+                }, status=400)
+
             serializer = FileListSerializer(data=data)
 
             if serializer.is_valid():
@@ -26,7 +34,7 @@ class HandleFileUpload(APIView):
                 return Response({
                     'status': 'success',
                     'message': 'File is ready to share.',
-                    'data' : serializer.data
+                    'data': serializer.data
                 })
             return Response({
                 'status': 'error',
@@ -35,20 +43,29 @@ class HandleFileUpload(APIView):
             })
         except Exception as e:
             print("Error in file upload view", str(e))
+            return Response({
+                'status': 'error',
+                'message': 'Internal server error',
+            }, status=500)
+
+
+from rest_framework import status
 
 class HandleFileDownload(APIView):
+    # ...
+
     def get(self, request, uid):
         folder = get_object_or_404(Folder, uid=uid)
         zip_file_path = os.path.join(settings.MEDIA_ROOT, 'zip', f'{uid}.zip')
 
         if not os.path.exists(zip_file_path):
             folder_files = folder.files_set.all()
+            if not folder_files:
+                return Response({
+                    'status': 'error',
+                    'message': 'No files found in the folder',
+                }, status=status.HTTP_404_NOT_FOUND)
+
             self.zip_files(folder_files, zip_file_path)
 
         return FileResponse(open(zip_file_path, 'rb'), as_attachment=True)
-
-    def zip_files(self, files, zip_file_path):
-        with zipfile.ZipFile(zip_file_path, 'w') as zip_file:
-            for file in files:
-                file_path = os.path.join(settings.MEDIA_ROOT, str(file.file))
-                zip_file.write(file_path, os.path.basename(file_path))
